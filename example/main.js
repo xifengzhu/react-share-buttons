b0a893f9f91110af016f81c049e8b9b84ddff214
Wu <wuzhaoyang@meizu.com>
2016-03-30 14:50:16 +0800

    update example

--- example/main.js
+++ example/main.js
@@ -7,7 +7,8 @@ class App extends React.Component {
     return (
       <div>
         <ShareButtons 
-          sites={['qq', 'wechat']}
+          sites={["qzone", "weibo", "google", "twitter", "qq", 
+          "tencent", "wechat", "douban", "linkedin", "facebook"]}
           url = "https://github.com/DawnyWu/react-share-buttons"
           title = "react-share-buttons"
           description = "一键分享到微博、QQ空间、QQ好友、微信、腾讯微博、豆瓣、Facebook、Twitter、Linkedin、Google+ 的 react 组件"
